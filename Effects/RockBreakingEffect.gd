72158d9b968cc9792bfa3c64e698b0d0425e23f4
Mudderhino <skymudz@gmail.com>
2021-05-24 20:17:54 +0200

    Added Documentation to new Additions

--- Effects/RockBreakingEffect.gd
+++ Effects/RockBreakingEffect.gd
@@ -3,8 +3,10 @@ extends Node2D
 
 onready var animatedSprite = get_node("AnimatedSprite")
 
+"""Plays the Animation"""
 func _ready():
 	animatedSprite.play("Animate")
-
+	
+"""Removes the Animation from the World when it has finished"""
 func _on_AnimatedSprite_animation_finished():
 	queue_free()
