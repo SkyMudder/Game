7b874811e7df7269ada9e4cd78061d24771db96d
Mudderhino <skymudz@gmail.com>
2021-05-25 18:43:25 +0200

    Should have been in last Commit

--- Inventory/Inventory.gd
+++ Inventory/Inventory.gd
@@ -5,7 +5,7 @@ onready var InventorySlot = load("res://Inventory/InventorySlot.gd")
 var inventorySlots = []
 
 func add(resourceType, resourceAmount):
-	for x in inventorySlots:
+	for x in inventorySlots:	# Make a new Slot if Resource is not already in the Inventory
 		if resourceType == x.getResourceType():
 			x.setResourceAmount(resourceAmount)
 			return
