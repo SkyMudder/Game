a530dc1512fa551d7fa70acc18e182c51a2d8f47
Mudderhino <skymudz@gmail.com>
2021-06-09 12:05:17 +0200

    Objects now emit a small Amount of Particles of the Objects Color when damaged since they have HP. The Object gets damaged while LMB is held on it. The explosive Effect will be emitted when the HP has reached 0. Changed the Scale of the Particles to 1.5 instead of 1.

--- Scenes/Tree.gd
+++ Scenes/Tree.gd
@@ -5,20 +5,47 @@ onready var inventory = Inventories.playerInventory
 onready var sprite = $Tree
 onready var collision = get_node("CollisionShape2D")
 onready var breakingEffect = get_node("Tree/BreakingEffect")
+onready var damagingEffect = get_node("Tree/DamagingEffect")
+
+var hp = 400
 
 var item = preload("res://Items/Wood.tres")
 var amount = 1
 var exists = 1
 
-"""Creates an Effect and makes the Object disappear
-when the Object gets left clicked"""
+func _ready():
+	set_process(false)
+	
+"""While active, removes HP
+Unbound from Framerate
+When HP reach 0, the Particles stop emitting and the Object gets destroyed"""
+func _process(delta):
+	hp -= 60 * delta
+	if hp < 0:
+		damagingEffect.emitting = false
+		destroy()
+
+"""While LMB is held on the Object, it gets damaged
+Meaning the _process Method is active
+Emits Particles with the Colors of the Object while damaged"""
 func _on_Hurtbox_input_event(_viewport, _event, _shape_idx):
 	if Input.is_mouse_button_pressed(BUTTON_LEFT):
-		if exists == 1:
+		set_process(true)
+		damagingEffect.emitting = true
+	else:
+		damagingEffect.emitting = false
+		set_process(false)
+	
+"""On Destroy, the Resource of the Object gets added to the Inventory
+A new explosive Particle Effect gets emitted
+Texture and Collision of the Object get deactivated on Particel Emission
+The Object gets freed after the time the Particle Effect needs to process"""
+func destroy():
+	if exists == 1:
 			inventory.add(item)
+			exists = 0
 			sprite.texture = null
 			collision.shape = null
 			breakingEffect.emitting = true
-			exists = 0
-			yield(get_tree().create_timer(breakingEffect.lifetime + 0.1), "timeout")
+			yield(get_tree().create_timer(breakingEffect.lifetime), "timeout")
 			queue_free()
